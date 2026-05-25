1f85f978c5c9295a120f3cec73723a15fe4ec4f9
zoogies <ryanzmuda@gmail.com>
2026-05-25 16:30:21 -0400

    argc argv

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::process::ExitCode;
+
+fn print_usage() {
+    println!("usage: alv <input file>.alv");
+}
+
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    let argc = args.len();
+
+    // for now just take one file as input
+    if argc < 2 || argc >= 3 {
+        print_usage();
+        return ExitCode::FAILURE
+    }
+
+    return ExitCode::SUCCESS
 }
